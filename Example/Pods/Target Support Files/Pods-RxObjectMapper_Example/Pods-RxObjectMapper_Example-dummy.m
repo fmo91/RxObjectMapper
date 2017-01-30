f6daeafa42e9eb6cd364d192f28083d8d3b13f01
Fernando <fernando-chan@MacBook-Pro-de-Fernando.local>
2017-01-30 10:30:33 -0300

    Works fine

---
+++ Example/Pods/Target Support Files/Pods-RxObjectMapper_Example/Pods-RxObjectMapper_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxObjectMapper_Example : NSObject
+@end
+@implementation PodsDummy_Pods_RxObjectMapper_Example
+@end
--- Example/RxObjectMapper/ViewController.swift
+++ Example/RxObjectMapper/ViewController.swift
@@ -7,12 +7,56 @@
 //
 
 import UIKit
+import RxObjectMapper
+import RxSwift
+import RxCocoa
+import ObjectMapper
+
+struct Post: Mappable {
+    var id: Int?
+    var title: String = ""
+    
+    init?(map: Map) {}
+    
+    mutating func mapping(map: Map) {
+        id <- map["id"]
+        title <- map["title"]
+    }
+}
 
 class ViewController: UIViewController {
+    let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        URLSession.shared
+            .rx.json(url: URL(string: "https://jsonplaceholder.typicode.com/posts")!)
+            .mapArray(type: Post.self)
+            .map { $0.map { $0.title } }
+            .subscribe(
+                onNext: { titles in
+                    print("posts => \(titles)")
+                },
+                onError: { _ in
+                    print("Error")
+                }
+            )
+            .addDisposableTo(disposeBag)
+        
+        URLSession.shared
+            .rx.json(url: URL(string: "https://jsonplaceholder.typicode.com/posts/1")!)
+            .mapObject(type: Post.self)
+            .subscribe(
+                onNext: { (post: Post) in
+                    print("Post title => \(post.title)")
+                },
+                onError: { _ in
+                    print("Error")
+                }
+            )
+            .addDisposableTo(disposeBag)
     }
 
     override func didReceiveMemoryWarning() {
